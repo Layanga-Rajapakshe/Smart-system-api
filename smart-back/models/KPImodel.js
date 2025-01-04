a2c7f68ce4d7df57ef09faad2127a155e4f7bc45
Tharindu <nbandara396@gmail.com>
2025-01-04 14:09:52 +0530

    updated KPI

--- smart-back/models/KPImodel.js
+++ smart-back/models/KPImodel.js
@@ -15,27 +15,27 @@ const kpiSchema = new mongoose.Schema({
         attitude: [{
             parameter: { type: String, required: true },
             weight: { type: Number, min: 0, max: 1, required: true }, // Weight between 0 and 1
-            value: { type: Number, min: 0, max: 5, required: true }
+            value: { type: Number, min: 0, max: 10, required: true }
         }],
         habits: [{
             parameter: { type: String, required: true },
             weight: { type: Number, min: 0, max: 1, required: true },
-            value: { type: Number, min: 0, max: 5, required: true }
+            value: { type: Number, min: 0, max: 10, required: true }
         }],
         skills: [{
             parameter: { type: String, required: true },
             weight: { type: Number, min: 0, max: 1, required: true },
-            value: { type: Number, min: 0, max: 5, required: true }
+            value: { type: Number, min: 0, max: 10, required: true }
         }],
         performance: [{
             parameter: { type: String, required: true },
             weight: { type: Number, min: 0, max: 1, required: true },
-            value: { type: Number, min: 0, max: 5, required: true }
+            value: { type: Number, min: 0, max: 10, required: true }
         }],
         knowledge: [{
             parameter: { type: String, required: true },
             weight: { type: Number, min: 0, max: 1, required: true },
-            value: { type: Number, min: 0, max: 5, required: true }
+            value: { type: Number, min: 0, max: 10, required: true }
         }]
     },
     month: {
