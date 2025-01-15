0f217b4871258e598f7d664e5b0ff044f284149d
Tharindu <nbandara396@gmail.com>
2025-01-15 08:29:58 +0530

    new

--- smart-back/routes/MeetingRoute.js
+++ smart-back/routes/MeetingRoute.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { getAllMeetings, getMeetingById, createMeeting, updateMeeting, deleteMeeting, addDiscussionPoints,getMeetingsByProjectId } = require("../controllers/MeetingController");
 const checkPermissionMiddleware = require('../middleware/CheckPermission');
+const authenticateUser = require('../middleware/AuthenticateUser');
 // Get all meetings
-router.get("/", getAllMeetings);
-router.get("/:id", getMeetingById);
-router.post("/", createMeeting);
-router.put("/:id", updateMeeting);
-router.delete("/:id", deleteMeeting);
-router.patch("/:id/discussion-points", addDiscussionPoints);
-router.get("/project/:projectId", getMeetingsByProjectId);
+router.get("/",authenticateUser, getAllMeetings);
+router.get("/:id",authenticateUser, getMeetingById);
+router.post("/",authenticateUser, createMeeting);
+router.put("/:id",authenticateUser, updateMeeting);
+router.delete("/:id",authenticateUser, deleteMeeting);
+router.patch("/:id/discussion-points",authenticateUser, addDiscussionPoints);
+router.get("/project/:projectId",authenticateUser, getMeetingsByProjectId);
 
 module.exports = router;
--- smart-back/routes/ProjectRoute.js
+++ smart-back/routes/ProjectRoute.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const {  createProject,getAllProjects,getProjectById,updateProject,deleteProject} = require("../controllers/ProjectController");
+const checkPermissionMiddleware = require('../middleware/CheckPermission');
+const authenticateUser = require('../middleware/AuthenticateUser');
 
 // Routes for the Project model
-router.post("/", createProject); // Create a new project
-router.get("/", getAllProjects); // Get all projects
-router.get("/:id", getProjectById); // Get project by ID
-router.put("/:id", updateProject); // Update a project
-router.delete("/:id", deleteProject); // Delete a project
+router.post("/",authenticateUser, createProject); // Create a new project
+router.get("/",authenticateUser, getAllProjects); // Get all projects
+router.get("/:id",authenticateUser, getProjectById); // Get project by ID
+router.put("/:id",authenticateUser, updateProject); // Update a project
+router.delete("/:id",authenticateUser, deleteProject); // Delete a project
 
 module.exports = router;
