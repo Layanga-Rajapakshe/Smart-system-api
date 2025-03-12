db17bf33fdbd998ebb4756b8fe559222b5422969
Tharindu <nbandara396@gmail.com>
2025-03-13 02:18:19 +0530

    new middleware

--- smart-back/middleware/AuthenticateUser.js
+++ smart-back/middleware/AuthenticateUser.js
@@ -2,21 +2,23 @@ const jwt = require('jsonwebtoken');
 const Employee = require('../models/EmployeeModel');
 const logger = require('../utils/Logger');
 
+const SUPER_ADMIN_ID = '66fbb15030e37b523885f5ad';
+
 const authenticateUser = async (req, res, next) => {
     try {
-        const token = req.cookies.auth_token; // Get the token from the cookie
+        const token = req.cookies.auth_token;
         if (!token) {
             throw new Error('Authentication token not found');
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await Employee.findById(decoded._id);
+        const user = await Employee.findById(decoded._id).populate('company');
 
         if (!user) {
             throw new Error('User not found');
         }
 
-        req.user = user;
+        req.user = user; // Attach user to request
         logger.log(`User authenticated: ${user.email}`);
         next();
     } catch (error) {
@@ -25,4 +27,29 @@ const authenticateUser = async (req, res, next) => {
     }
 };
 
-module.exports = authenticateUser;
+
+const authorizeCompanyAccess = async (req, res, next) => {
+    try {
+        if (req.user._id.toString() === SUPER_ADMIN_ID) {
+            return next(); // Super Admin has full access
+        }
+
+        
+        const companyId = req.params.companyId || req.body.company; // Extract company ID from params or body
+
+        if (!companyId) {
+            return res.status(400).json({ message: 'Company ID is required' });
+        }
+
+        if (req.user.company._id.toString() === companyId.toString()) {
+            return next(); // User belongs to the company, allow access
+        }
+
+        return res.status(403).json({ message: 'Access denied: Unauthorized company' });
+    } catch (error) {
+        logger.error(`Authorization error: ${error.message}`);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+module.exports = { authenticateUser, authorizeCompanyAccess };
