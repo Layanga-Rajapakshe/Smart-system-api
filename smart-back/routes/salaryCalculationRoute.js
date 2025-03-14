718cd97e9a47358e5bae798fb0c0661f9bd7e497
Akila-Piyumantha <akila7976ap@gmail.com>
2025-03-14 19:53:31 +0530

    added

--- smart-back/routes/salaryCalculationRoute.js
+++ smart-back/routes/salaryCalculationRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const {calculateSalary } = require('../controllers/SalaryCalculationController');
+const {calculateSalary,showsalarysheet } = require('../controllers/SalaryCalculationController');
 const checkPermissionMiddleware = require('../middleware/CheckPermission');
 const {authenticateUser} = require('../middleware/AuthenticateUser');
 const router = express.Router();
 
 router.post('/sketch' ,  calculateSalary);
+router.get('/showsal',showsalarysheet);
 
 module.exports = router;
