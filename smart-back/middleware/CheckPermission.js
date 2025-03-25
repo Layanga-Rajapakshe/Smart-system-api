518317446f5e49e3371151582ccd378bea178bd0
Tharindu <nbandara396@gmail.com>
2025-03-25 17:56:15 +0530

    updated permission

--- smart-back/middleware/CheckPermission.js
+++ smart-back/middleware/CheckPermission.js
@@ -4,13 +4,18 @@ const checkPermissionMiddleware = (requiredPermission) => {
     return async (req, res, next) => {
         try {
             const employee = await Employee.findById(req.user._id).populate('role');
-            const role = employee.role;
+            if (!employee || !employee.role) {
+                return res.status(403).json({ message: "Role not found." });
+            }
+            
+            const { permissions, temporaryPermission } = employee.role;
 
-            if (!role.permissions.includes(requiredPermission)) {
-                return res.status(403).json({ message: `You do not have permission to ${requiredPermission}` });
+            if (permissions.includes(requiredPermission) || temporaryPermission.includes(requiredPermission)) {
+                return next(); 
             }
 
-            next();
+            return res.status(403).json({ message: `You do not have permission to ${requiredPermission}` });
+
         } catch (error) {
             return res.status(500).json({ message: error.message });
         }
